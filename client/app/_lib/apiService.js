5f49c9def6565a534954a4ccdc3437005f9495ff
SalahJamal1 <salahabufarha1996@outlook.com>
2025-05-19 10:01:14 +0300

    deployment

--- client/app/_lib/apiService.js
+++ client/app/_lib/apiService.js
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 const { default: axios } = require("axios");
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8080/api/v1/",
+  baseURL: "https://wild-56d7d47b4341.herokuapp.com/api/v1/",
   withCredentials: true,
 });
 
