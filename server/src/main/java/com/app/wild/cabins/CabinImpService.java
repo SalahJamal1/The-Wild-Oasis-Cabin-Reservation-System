d3398d92bb4f3308421ac1501871007158f62b50
SalahJamal1 <salahabufarha1996@outlook.com>
2025-08-03 06:54:20 +0300

    Update CabinImpService.java

--- server/src/main/java/com/app/wild/cabins/CabinImpService.java
+++ server/src/main/java/com/app/wild/cabins/CabinImpService.java
@@ -11,11 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-
 public class CabinImpService extends GenericService<Cabin,Integer> {
-    @Autowired
 public CabinImpService(CabinRepository cabinRepository){
         super(cabinRepository);
     }
-
 }
